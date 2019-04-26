3371e78dc9e6c358c5e3f30c45833c65d88f464e
altruistcoder <rishabhaggarwal0101@gmail.com>
2019-04-27 03:53:49 +0530

    Elements Pair with Given Sum Using Hashing

---
+++ pair_with_given_sum_hashing.py
@@ -0,0 +1,15 @@
+def findPair(a, n, s):
+    # Creating an empty hash set
+    h = set()
+
+    for i in range(0, n):
+        temp = s - a[i]
+        if temp >= 0 and temp in h:
+            print("Pair with the given sum is", temp, "and", a[i])
+        h.add(a[i])
+    print(h)
+
+
+a = list(map(int, input("Enter the values of the array: ").split()))
+s = int(input("Enter the value of sum to be find: "))
+findPair(a, len(a), s)
